14f4d1672a91cc386f10e0d8b966ccb9afa2cc2d
Aram Ceballos <aramgonzalez12@hotmail.com>
2021-07-26 19:09:08 -0700

    :white_check_mark: Add tests for users and favorites

--- test/app_test.go
+++ test/app_test.go
@@ -124,6 +124,70 @@ func TestCategoriesRoute(t *testing.T) {
 	}
 }
 
+func TestUsersRoute(t *testing.T) {
+
+	tests := []struct {
+		description   string
+		route         string
+		expectedError bool
+		expectedCode  int
+	}{
+		{
+			description:   "user route",
+			route:         "/api/user?id=3",
+			expectedError: false,
+			expectedCode:  200,
+		},
+	}
+
+	app := fiber.New(fiber.Config{
+		CaseSensitive: true,
+	})
+	app.Use(cors.New())
+
+	err := godotenv.Load("../.env")
+	if err != nil {
+		log.Fatalf("Error loading .env file: %s", err)
+	}
+
+	api := app.Group("/api")
+
+	routes.AuthRouter(api)
+	routes.UsersRouter(api)
+
+	token := getToken(app)
+
+	for _, test := range tests {
+		req, _ := http.NewRequest(
+			"GET",
+			test.route,
+			nil,
+		)
+		req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", token))
+
+		res, err := app.Test(req, -1)
+
+		assert.Equalf(t, test.expectedError, err != nil, test.description)
+
+		if test.expectedError {
+			continue
+		}
+
+		assert.Equalf(t, test.expectedCode, res.StatusCode, test.description)
+
+		b, err := ioutil.ReadAll(res.Body)
+		if err != nil {
+			log.Fatalf("could not read response: %v", err)
+		}
+
+		var got map[string]interface{}
+		err = json.Unmarshal(b, &got)
+		if err != nil {
+			log.Fatalf("could not unmarshall response %v", err)
+		}
+	}
+}
+
 func TestPostsRoute(t *testing.T) {
 
 	tests := []struct {
@@ -141,6 +205,12 @@ func TestPostsRoute(t *testing.T) {
 		{
 			description:   "post route",
 			route:         "/api/post/18",
+			expectedError: false,
+			expectedCode:  200,
+		},
+		{
+			description:   "favorites route",
+			route:         "/api/posts/favorites",
 			expectedError: false,
 			expectedCode:  200,
 		},
