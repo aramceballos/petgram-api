f3e9fa124c9e357388437d0199e063b9028a70e6
Aram Ceballos <aramgonzalez12@hotmail.com>
2021-07-26 18:57:10 -0700

    :white_check_mark: Add test for categories routes

--- go.mod
+++ go.mod
@@ -11,6 +11,7 @@ require (
 	github.com/gofiber/jwt/v2 v2.2.1
 	github.com/joho/godotenv v1.3.0
 	github.com/klauspost/compress v1.12.2 // indirect
+	github.com/stretchr/testify v1.7.0 // indirect
 	github.com/valyala/fasthttp v1.24.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
 	golang.org/x/crypto v0.0.0-20210322153248-0c34fe9e7dc2
---
+++ test/app_test.go
@@ -0,0 +1,127 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"testing"
+
+	"github.com/aramceballos/petgram-api/api/routes"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/joho/godotenv"
+	"github.com/stretchr/testify/assert"
+)
+
+func getToken(app *fiber.App) string {
+
+	type data struct {
+		Token string
+	}
+	type resBody struct {
+		Data    data
+		Message string
+	}
+
+	postBody, _ := json.Marshal(map[string]string{
+		"identity": "tester",
+		"password": "test",
+	})
+
+	req, _ := http.NewRequest(
+		"POST",
+		"/api/login",
+		bytes.NewBuffer(postBody),
+	)
+	req.Header.Set("Content-Type", "application/json")
+
+	res, _ := app.Test(req, -1)
+
+	b, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		log.Fatalf("could not read response: %v", err)
+	}
+
+	var got resBody
+	err = json.Unmarshal(b, &got)
+	if err != nil {
+		log.Fatalf("could not unmarshall response %v", err)
+	}
+
+	return got.Data.Token
+}
+
+func TestCategoriesRoute(t *testing.T) {
+
+	tests := []struct {
+		description   string
+		route         string
+		expectedError bool
+		expectedCode  int
+	}{
+		{
+			description:   "categories route",
+			route:         "/api/categories",
+			expectedError: false,
+			expectedCode:  200,
+		},
+		{
+			description:   "category route",
+			route:         "/api/category/1",
+			expectedError: false,
+			expectedCode:  200,
+		},
+	}
+
+	app := fiber.New(fiber.Config{
+		CaseSensitive: true,
+	})
+	app.Use(cors.New())
+
+	err := godotenv.Load("../.env")
+	if err != nil {
+		log.Fatalf("Error loading .env file: %s", err)
+	}
+
+	api := app.Group("/api")
+
+	routes.AuthRouter(api)
+	routes.CategoriesRouter(api)
+
+	token := getToken(app)
+
+	for _, test := range tests {
+		req, _ := http.NewRequest(
+			"GET",
+			test.route,
+			nil,
+		)
+		req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", token))
+
+		res, err := app.Test(req, -1)
+
+		assert.Equalf(t, test.expectedError, err != nil, test.description)
+
+		if test.expectedError {
+			continue
+		}
+
+		assert.Equalf(t, test.expectedCode, res.StatusCode, test.description)
+
+		b, err := ioutil.ReadAll(res.Body)
+		if err != nil {
+			log.Fatalf("could not read response: %v", err)
+		}
+
+		var got map[string]interface{}
+		err = json.Unmarshal(b, &got)
+		if err != nil {
+			log.Fatalf("could not unmarshall response %v", err)
+		}
+
+		fmt.Println(got["message"])
+	}
+}
